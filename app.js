27ee3141732c85aa1162dc02819d2d4f8c3558b9
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-06 16:09:02 +0100

    Add basic style

---
+++ app.js
@@ -0,0 +1,7 @@
+const buttonEl = document.getElementById("add-button")
+const inputEl = document.getElementById("input-field")
+
+buttonEl.addEventListener("click", function () {
+    let inputValue = inputEl.value
+    console.log(inputValue)
+})
