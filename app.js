12912c37ea359a87bde55bd7f450072c7ce5acd4
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-06 21:25:06 +0100

    Add fetch data from db code. Add style.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-c8093-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -9,7 +9,18 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const itemsInDB = ref(database, "items")
 
-console.log(app)
+onValue(itemsInDB, (snapshot) => {
+        let itemsArray = Object.values(snapshot.val())
+
+        let itemList = ""
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            itemList += `<li>${currentItem}</li>`
+        }
+
+        shoppingListEl.innerHTML = itemList
+    })
 
 const buttonEl = document.getElementById("add-button")
 const inputEl = document.getElementById("input-field")
@@ -22,13 +33,13 @@ buttonEl.addEventListener("click", function () {
 
     clearInputEl()
 
-    appendItemToShoppingListEl(inputValue) 
+    // appendItemToShoppingListEl(inputValue) 
 })
 
 function clearInputEl() {
     inputEl.value = ""
 }
 
-function appendItemToShoppingListEl(itemValue) {
-    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
-}
+// function appendItemToShoppingListEl(itemValue) {
+//     shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+// }
