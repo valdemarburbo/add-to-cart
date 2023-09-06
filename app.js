e80438e546551043ba7eef80dd641678cacb3f62
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-06 17:07:33 +0100

    Add list render on button click

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-c8093-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const itemsInDB = ref(database, "items")
+
+console.log(app)
+
 const buttonEl = document.getElementById("add-button")
 const inputEl = document.getElementById("input-field")
+const shoppingListEl = document.getElementById("shopping-list")
 
 buttonEl.addEventListener("click", function () {
     let inputValue = inputEl.value
-    console.log(inputValue)
+
+    push(itemsInDB, inputValue)
+
+    console.log(`${inputValue} added to database`)
+
+    inputEl.value = ""
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
+
+    
 })
